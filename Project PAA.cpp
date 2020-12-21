494626b747fd88db345b5f2c96cbbf44f1b83d63
fahrysholahuddin <fahrysholahuddin@users.noreply.github.com>
2020-12-21 14:01:32 +0700

    Menampilkan data buah grade A

--- Project PAA.cpp
+++ Project PAA.cpp
@@ -11,6 +11,13 @@ struct buah{
 	int kondisi;
 }dataBuah[30];
 
+struct buahGradeA{
+	string nama;
+	int no;
+	float berat;
+	int kondisi;
+}dataBuahGradeA[30];
+
 int main(){
 	cout << "Program Sortir Buah" << endl;
 	cout << "Masukkan nama buah : ";
@@ -20,27 +27,44 @@ int main(){
 	cout << "=========================";
 	for (int i = 0; i < jumlahBuah; i++){
 		dataBuah[i].no += 1;
-		cout << endl << "Berat buah " << dataBuah[0].nama << " ke-" << i+1 << " = ";
+		cout << endl << "Berat buah " << dataBuah[0].nama << " per pack ke-" << dataBuah[i].no << " = ";
 		cin >> dataBuah[i].berat;
-		cout << "Kondisi buah " <<dataBuah[0].nama << " ke-" << i+1 << " (0-5) = ";
+		cout << "Kondisi buah " <<dataBuah[0].nama << " per pack ke-" << dataBuah[i].no << " (0-5) = ";
 		cin >> dataBuah[i].kondisi;
 	}
-	
+
 	for (int i = 0; i < jumlahBuah; i++){
 		if (dataBuah[i].kondisi < 1){
 			gradeA += 1;
+			dataBuah[0].nama = dataBuahGradeA[i].nama;
+			dataBuah[i].no = dataBuahGradeA[i].no;
+			dataBuah[i].kondisi = dataBuahGradeA[i].kondisi;
+			dataBuah[i].berat = dataBuahGradeA[i].berat;
 		}else if (dataBuah[i].kondisi >= 1 && dataBuah[i].kondisi <= 2){
 			gradeB += 1;
 		}
 	}
-	
+
 	for (int i = 0; i < jumlahBuah; i++){
 		totalJumlahKotor += 1;
 		totalBeratKotor = totalBeratKotor + dataBuah[i].berat;
 	}
+
+	for(int i = 0; i < jumlahBuah; i++){
+
+	}
 	cout << endl << "=========================";
 	cout << endl << "Bruto jumlah seluruh buah = "<< totalJumlahKotor;
 	cout << endl << "Bruto berat seluruh buah = " << totalBeratKotor;
-	cout << endl << "Jumlah Buah Grade A = " << gradeA;
+	cout << endl << "Jumlah Buah Grade A (Layak Jual) = " << gradeA << endl ;
+	cout << endl << "Daftar buah layak jual : ";
+	for (int i = 0; i < gradeA ; i++){
+            cout << dataBuahGradeA[i].nama;
+			dataBuahGradeA[i].no;
+			dataBuahGradeA[i].kondisi;
+			dataBuahGradeA[i].berat;
+	}
+
+
 	cout << endl << "Jumlah Buah Grade B = " << gradeB;
 }
