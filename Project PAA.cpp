c66323d52f0a9f4eedc1a6576234ad7b271393b3
fahrysholahuddin <fahrysholahuddin@users.noreply.github.com>
2021-01-19 13:52:02 +0700

    Penambahan serta perbaikan Selection Sort

--- Project PAA.cpp
+++ Project PAA.cpp
@@ -35,18 +35,39 @@ int main()
     cin >> dataBuah[0].nama;
     cout << "Masukan jumlah pack buah = ";
     cin >> jumlahBuah;
-    cout << "=========================";
+    cout << endl << "=========================\n\n";
     for (int i = 0; i < jumlahBuah; i++)
     {
         dataBuah[i].no = dataBuah[i - 1].no + 1;
-        cout << endl << "Berat buah " << dataBuah[0].nama << " per pack ke-" << dataBuah[i].no << " = ";
+        cout << "Berat buah " << dataBuah[0].nama << " No.Pack.ID : " << dataBuah[i].no << " = ";
         cin >> dataBuah[i].berat;
-        cout << "Jumlah buah jelek " << dataBuah[0].nama << " per pack ke-" << dataBuah[i].no << " (0-1) = ";
+        cout << "Jumlah buah jelek " << " = ";
         cin >> dataBuah[i].kondisi;
     }
-
     for (int i = 0; i < jumlahBuah; i++)
     {
+        int min_idx = i;
+        for (int j = i+1; j < jumlahBuah; j++)
+            if (dataBuah[j].berat < dataBuah[min_idx].berat)
+                min_idx = j;
+
+        string tempNama;
+        tempNama = dataBuah[min_idx].nama;
+        dataBuah[min_idx].nama = dataBuah[i].nama;
+        dataBuah[i].nama = tempNama;
+
+        int tempNo = dataBuah[min_idx].no;
+        dataBuah[min_idx].no = dataBuah[i].no;
+        dataBuah[i].no = tempNo;
+
+        int tempBerat = dataBuah[min_idx].berat;
+        dataBuah[min_idx].berat = dataBuah[i].berat;
+        dataBuah[i].berat = tempBerat;
+
+        int tempKondisi = dataBuah[min_idx].kondisi;
+        dataBuah[min_idx].kondisi = dataBuah[i].kondisi;
+        dataBuah[i].kondisi = tempKondisi;
+
         if (dataBuah[i].kondisi == 0)
         {
             gradeA += 1;
@@ -63,38 +84,49 @@ int main()
             dataBuahGradeB[gradeB-1].kondisi = dataBuah[i].kondisi;
             dataBuahGradeB[gradeB-1].berat = dataBuah[i].berat;
         }
-    }
-
-    for (int i = 0; i < jumlahBuah; i++)
-    {
         totalJumlahKotor += 1;
         totalBeratKotor = totalBeratKotor + dataBuah[i].berat;
         totalBeratGradeA = totalBeratGradeA + dataBuahGradeA[i].berat;
         totalBeratGradeB = totalBeratGradeB + dataBuahGradeB[i].berat;
     }
 
-    cout << endl << "=========================";
-    cout << endl << "Daftar buah keseluruhan " << endl;
+    cout << endl << "=========================\n";
+    cout << endl << "Daftar buah keseluruhan \n" << endl;
     for (int i = 0; i < jumlahBuah; i++)
     {
-        cout << "Nama buah : " << dataBuah[0].nama << " no = " << dataBuah[i].no << " kodisi : " << dataBuah[i].kondisi << endl;
+        if (dataBuah[i].kondisi == 0)
+        {
+           cout << "# no.ID = " << dataBuah[i].no << "A # Kondisi : Bagus " << " # berat : " << dataBuah[i].berat << " Kg" << endl;
+        }
+        else if (dataBuah[i].kondisi > 0)
+        {
+            cout << "# no.ID = " << dataBuah[i].no << "B # Kondisi : Jelek " << " # berat : " << dataBuah[i].berat << " Kg" << endl;
+        }
+
     }
     cout << endl << "Jumlah buah keseluruhan = " << totalJumlahKotor << " pack";
     cout << endl << "Jumlah berat buah keseluruhan = " << totalJumlahKotor << " pack";
-    cout << endl << "=========================";
-    cout << endl << "Daftar buah layak jual : " << endl;
+    cout << endl << "\n=========================\n";
+    cout << endl << "Daftar buah " << dataBuahGradeA[0].nama << " layak jual : \n" << endl;
+
     for (int i = 0; i < gradeA; i++)
     {
-        cout << "Nama buah : " << dataBuahGradeA[0].nama << " no.ID = " << dataBuahGradeA[i].no << " kodisi : " << dataBuahGradeA[i].kondisi << " Berat : " << dataBuahGradeA[i].berat << endl;
+        cout << i+1 << "# no.ID = " << dataBuahGradeA[i].no << "A # kodisi : Bagus" << " # Berat : " << dataBuahGradeA[i].berat << " Kg" << endl;
+
     }
+
     cout << endl << "Jumlah Buah Grade A (Layak Jual) = " << gradeA << " pack";
     cout << endl << "Total Berat Buah Grade A (Layak Jual) = " << totalBeratGradeA << " Kg";
-    cout << endl << "=========================\n";
-    cout << endl << "Daftar buah tidak layak jual : " << endl;
+    cout << endl << "\n=========================\n";
+    cout << endl << "Daftar buah " << dataBuahGradeB[0].nama << " tidak layak jual : \n" << endl;
+
     for (int i = 0; i < gradeB; i++)
     {
-            cout << "Nama buah : " << dataBuahGradeB[0].nama << " noID = " << dataBuahGradeB[i].no << " kodisi : " << dataBuahGradeB[i].kondisi << " Berat : " << dataBuahGradeB[i].berat << endl;
+        cout << i+1 << "# noID = " << dataBuahGradeB[i].no << "B # Jumlah Buah Jelek : "<< dataBuahGradeB[i].kondisi << " # Berat : " << dataBuahGradeB[i].berat << endl;
     }
     cout << endl << "Jumlah Buah Grade B (Tidak Layak Jual) = " << gradeB << " pack";
     cout << endl << "Total Berat Buah Grade B (Tidak Layak Jual) = " << totalBeratGradeB << " Kg ";
+    cout << endl << "\n=========================\n\n";
+
 }
+
